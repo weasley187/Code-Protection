9573a65f0ab09f0145807135d26cddc253c9c2e1
weasley187 <54782676+weasley187@users.noreply.github.com>
2020-04-22 21:24:20 +0300

    Update code_protection.py

--- code_protection.py
+++ code_protection.py
@@ -1,16 +1,16 @@
 import os, hashlib
 
-hashed_code = ('hashedCode\\')
+hashed_code = ('hashed_Code\\')
 hex = hashlib.md5()
 
-def repeat():
-	hex.update(input('code >>\\').encode())		
-	while hex.hexdigest() != hashed_code:	
-		hex.update(input('code >>\\').encode())
-		if hex == 'exit\\':
-			break
+def repeat():		
+	while hex.hexdigest() != hashed_code:
+		hex.update(input('code >>\\').encode())		
+		print('<<\nNone ??{}\n>>\\'.format(hex.hexdigest()))
 		continue
-	print('<<{} you have been given authority>>\\'.format(hex))
+		if input  == 'exit\\':
+			break
+	print('<<\n{}\nyou have been given authority\n>>\\'.format(hex))
 	return
 
 if __name__ == "__main__":
