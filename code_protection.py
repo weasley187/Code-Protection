2aa44e2cb72c004902165bdcde71cf53dc016b31
weasley187 <54782676+weasley187@users.noreply.github.com>
2020-04-22 21:37:06 +0300

    Update code_protection.py

--- code_protection.py
+++ code_protection.py
@@ -1,16 +1,20 @@
-import os, hashlib
+import os, sys, hashlib
 
-hashed_code = ('hashed_Code\\')
+hashed_code = ('32250170a0dca92d53ec9624f336ca24')
 hex = hashlib.md5()
 
-def repeat():		
+def repeat():
+	global hex
 	while hex.hexdigest() != hashed_code:
+		hex = hashlib.md5()			
 		hex.update(input('code >>\\').encode())		
 		print('<<\nNone ??{}\n>>\\'.format(hex.hexdigest()))
 		continue
 		if input  == 'exit\\':
+			sys.exit()
 			break
-	print('<<\n{}\nyou have been given authority\n>>\\'.format(hex))
+	print('<<\n{}\nyou have been given authority\n>>\\'.format(hex.digest()))
+	os.system(hashed_code)
 	return
 
 if __name__ == "__main__":
